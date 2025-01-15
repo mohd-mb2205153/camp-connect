b36f1a393711418cb98a263f945a1f6bb85dda52
jalebyy <jalebyy@users.noreply.github.com>
2025-01-15 13:22:19 +0300

    google maps package

--- campconnect/lib/main.dart
+++ campconnect/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:campconnect/routes/app_router.dart';
+import 'package:campconnect/services/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,);
   runApp(ProviderScope(child: const MyApp()));
 }
 
