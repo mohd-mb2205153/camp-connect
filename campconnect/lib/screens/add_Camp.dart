29a421f2ddcd67b457ec51cab01afd73cb7d3450
Mohammed Mansib Shahriyar Alam <mohdmansib@gmail.com>
2025-01-16 20:39:47 +0300

    inital setcampLocation

--- campconnect/lib/screens/add_Camp.dart
+++ campconnect/lib/screens/add_Camp.dart
@@ -17,12 +17,15 @@ class AddCamp extends ConsumerStatefulWidget {
 class _AddCamp extends ConsumerState<AddCamp> {
   Completer<GoogleMapController> _googleMapController = Completer();
   late LatLng _default;
+  late LatLng _scrolledLocation;
   CameraPosition? _initial;
+  String addressStr = '';
 
   @override
   void initState() {
     _default = LatLng(25.3, 51.487);
-    _initial = CameraPosition(target: _default, zoom: 15);
+    _scrolledLocation = _default;
+    _initial = CameraPosition(target: _default, zoom: 17);
     _goToUserPostion();
 
     super.initState();
@@ -30,14 +33,22 @@ class _AddCamp extends ConsumerState<AddCamp> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _body(),
+    return SafeArea(
+      child: Scaffold(
+        body: _body(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _goToUserPostion();
+          },
+          child: Icon(Icons.location_on),
+        ),
+      ),
     );
   }
 
   Widget _body() {
     return Stack(
-      children: [_getMap()],
+      children: [_getMap(), _customPin(), showAddress()],
     );
   }
 
@@ -45,8 +56,13 @@ class _AddCamp extends ConsumerState<AddCamp> {
     return GoogleMap(
       initialCameraPosition: _initial!,
       mapType: MapType.hybrid,
-      onCameraIdle: () {},
-      onCameraMove: (position) {},
+      zoomControlsEnabled: false,
+      onCameraIdle: () {
+        getAddress(_scrolledLocation);
+      },
+      onCameraMove: (position) {
+        _scrolledLocation = position.target;
+      },
       onMapCreated: (GoogleMapController controller) {
         if (!_googleMapController.isCompleted) {
           _googleMapController.complete(controller);
@@ -57,13 +73,15 @@ class _AddCamp extends ConsumerState<AddCamp> {
 
   Future _goToUserPostion() async {
     Position currentPostion = await _userPosition();
-    _goToPosition(LatLng(currentPostion.latitude, currentPostion.longitude));
+    _goToScrolledPosition(
+        LatLng(currentPostion.latitude, currentPostion.longitude));
   }
 
-  Future _goToPosition(LatLng position) async {
+  Future _goToScrolledPosition(LatLng position) async {
     GoogleMapController googleMapController = await _googleMapController.future;
     googleMapController.animateCamera(CameraUpdate.newCameraPosition(
-        CameraPosition(target: position, zoom: 15)));
+        CameraPosition(target: position, zoom: 17)));
+    await getAddress(position);
   }
 
   Future _userPosition() async {
@@ -93,6 +111,34 @@ class _AddCamp extends ConsumerState<AddCamp> {
   }
 
   Widget _customPin() {
-    return Container(child: Lottie.asset('/'));
+    return Center(
+      child: SizedBox(
+        width: 70,
+        child: Lottie.asset('assets/icon/pin.json'),
+      ),
+    );
+  }
+
+  Widget showAddress() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      decoration: BoxDecoration(color: const Color.fromARGB(255, 5, 174, 152)),
+      child: Center(
+        child: Text('Set Location to $addressStr',
+            style: TextStyle(fontStyle: FontStyle.italic)),
+      ),
+    );
+  }
+
+  Future getAddress(LatLng position) async {
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    Placemark address = placemarks[0];
+    String fullAddress =
+        "${address.street},${address.locality},${address.administrativeArea},${address.country}";
+    setState(() {
+      addressStr = fullAddress;
+    });
   }
 }
