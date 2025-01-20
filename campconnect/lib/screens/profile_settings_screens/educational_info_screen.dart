990b156661e944f27dfc557424b3fea9d11d98b0
Fahrel <fahrelazki@gmail.com>
2025-01-20 23:40:02 +0300

    Update educational_info_screen.dart

--- campconnect/lib/screens/profile_settings_screens/educational_info_screen.dart
+++ campconnect/lib/screens/profile_settings_screens/educational_info_screen.dart
@@ -981,30 +981,3 @@ class AreaOfExpertiseSection extends StatelessWidget {
     );
   }
 }
-
-class DetailsSection extends StatelessWidget {
-  final String title;
-  final IconData icon;
-  final List<Widget> children;
-
-  const DetailsSection({
-    super.key,
-    required this.title,
-    required this.icon,
-    required this.children,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: SectionTitleWithIcon(
-        icon: icon,
-        title: title,
-        child: Column(
-          children: children,
-        ),
-      ),
-    );
-  }
-}
